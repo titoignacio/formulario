70724e11d057ba8db9c99023a27d6c8f599517da
titoignacio <h.villalobosm03@gmail.com>
2019-09-05 16:16:23 -0400

    segundo commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'sales/new'
+  post 'sales/create', as:'sales' ,to:'sales#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
