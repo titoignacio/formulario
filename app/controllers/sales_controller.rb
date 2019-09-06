44a58cc6bbc543d66f70c0092f6a34d73b59149e
titoignacio <h.villalobosm03@gmail.com>
2019-09-06 16:59:07 -0400

    params 2

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -3,13 +3,28 @@ class SalesController < ApplicationController
   end
 
   def create
+    sale = Sale.new(sales_params)
+     sale.total = sale.value.to_f * (1-(sale.discount.to_f/100))
+        if sale.tax == 0
+       sale.total = sale.total * 1.19
+       sale.tax = 19
+        else
+       sale.tax = 0
 
-  end
+        end
+      byebug
+      end
+
+
+
+
+
+
 
   private
 
   def sales_params
     params.require(:sales).permit(:cod, :detail, :category, :value, :discount, :tax)
-end
+  end
 
 end
