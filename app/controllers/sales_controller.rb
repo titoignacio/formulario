06de917d56088a2ed660824fe668386a68bdf7a2
titoignacio <h.villalobosm03@gmail.com>
2019-09-06 16:01:14 -0400

    params commit

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -1,4 +1,15 @@
 class SalesController < ApplicationController
   def new
   end
+
+  def create
+
+  end
+
+  private
+
+  def sales_params
+    params.require(:sales).permit(:cod, :detail, :category, :value, :discount, :tax)
+end
+
 end
--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,8 @@
 class Sale < ApplicationRecord
+validates :cod, uniqueness:true
+validates :detail, presence:true
+validates_inclusion_of :category, in: [1, 2, 3, 4]
+validates_numericality_of :value, only_integer: true
+validates :discount, numericality: {only_integer: true, lees_than_or_equal_to:40}
+
 end
